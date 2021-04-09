046f95904a71849ef70deaa0dc400b0c93e80e14
Bruno Rogerio Fernandes <brogeriofernandes@gmail.com>
2021-04-09 09:58:54 -0300

    Código inicial do tksamc

---
+++ run_pdg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+python2 ./tksamc.py -T $1 -ph $2 -s MC -f $3 > output.txt
+
+sed '/#/Id' Output*.dat | awk '{print $3" "$12}' > dG_Energy_`expr "$3" : "processed_\(.*\).pdb"`.dat
+
+zip ${4}.zip *.jpg *.dat *.pdb *. README.txt
+
+find . -type l -delete
+rm tksamc.exe
+
+touch finished
+
